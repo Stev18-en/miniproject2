e5c148cce15acf50f57401a5f5fa0bcbc6da5aca
Steven Marchy Simanjuntak <stevensmjtk17@gmail.com>
2026-05-12 21:23:00 +0700

    Membuat DAO setoran hafalan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.ksp)
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
@@ -49,10 +49,13 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.compose.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
@@ -62,5 +65,4 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-
 }
---
+++ app/src/main/java/com/stevenmarchy0013/simukmin/database/SetoranDAO.kt
@@ -0,0 +1,28 @@
+package com.stevenmarchy0013.simukmin.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.stevenmarchy0013.simukmin.model.Setoran
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SetoranDao {
+
+    @Insert
+    suspend fun insert(setoran: Setoran)
+
+    @Update
+    suspend fun update(setoran: Setoran)
+
+    @Delete
+    suspend fun delete(setoran: Setoran)
+
+    @Query("SELECT * FROM setoran ORDER BY id DESC")
+    fun getSetoran(): Flow<List<Setoran>>
+
+    @Query("SELECT * FROM setoran WHERE id = :id")
+    suspend fun getSetoranById(id: Long): Setoran?
+}
