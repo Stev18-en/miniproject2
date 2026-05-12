f0a2d646a50f98d993e9bb8f50eb968ab545566d
Steven Marchy Simanjuntak <stevensmjtk17@gmail.com>
2026-05-12 19:58:16 +0700

    Menambahkan konfigurasi Room Database

--- app/src/main/java/com/stevenmarchy0013/simukmin/model/Setoran.kt
+++ app/src/main/java/com/stevenmarchy0013/simukmin/model/Setoran.kt
@@ -1,7 +1,13 @@
 package com.stevenmarchy0013.simukmin.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "setoran")
 data class Setoran(
-    val id: Long,
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
     val namaSiswa: String,
     val surah: String,
     val ayat: String,
